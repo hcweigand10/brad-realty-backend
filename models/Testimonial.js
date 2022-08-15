8cc05d8b3620a8888095befbbfaf777a76d58f24
hcweigand10 <henryweigand10@gmail.com>
2022-08-15 10:50:42 -0700

    testimonail fit zillow

--- models/Testimonial.js
+++ models/Testimonial.js
@@ -13,30 +13,46 @@ Testimonial.init(
       primaryKey: true,
       autoIncrement: true
     },
+    zillow_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false
     },
-    city: {
+    date: {
       type: DataTypes.STRING,
       allowNull: false
     },
-    state: {
+    work_done: {
       type: DataTypes.STRING,
       allowNull: false
     },
-    review: {
+    review_body: {
       type: DataTypes.TEXT,
       allowNull: false,
     },
-    email: {
-      type: DataTypes.STRING,
+    local_knowledge: {
+      type: DataTypes.INTEGER,
       allowNull: false,
       unique: true,
-      validate: {
-        isEmail: true
-      }
-    }
+    },
+    process_expertise: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true,
+    },
+    responsiveness: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true,
+    },
+    negotiation_skills: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true,
+    },
   },
   {
     sequelize,
